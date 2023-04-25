f097b89140c5d345271cd0ee7bc24794b88b7bd0
Adarsh Raj <ahazadarsh0014@gmail.com>
2023-04-25 18:15:11 +0530

    first commit 9

--- Controllers/JwtController.js
+++ Controllers/JwtController.js
@@ -1,19 +1,24 @@
 import jwt from "jsonwebtoken";
-
+import UserModel from "../Model/userModel.js";
 
 export const createNew=async(req,res)=>{
    try {
+
       const {userId,username}=req.body
+
+      const user = await UserModel.findOne({ username: username });
+     if(user){
       const token = jwt.sign(
         {
-          username: username,
-          id: userId,
+          username: user.username,
+          id: user._id,
         },
         process.env.JWT_KEY,
         { expiresIn: "7d" }
       );
 
-      res.status(200).json({token})
+      res.status(200).json({ user, token });
+     }
    } catch (error) {
     res.status(500).json(error)
    }
